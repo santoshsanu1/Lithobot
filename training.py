1ee4a4f83a32b8e55a6c57fd3dec2238d0f6ba43
Santosh Kumar <62773681+santoshsanu1@users.noreply.github.com>
2022-04-10 22:10:53 +0530

    Training file uploaded

--- training.py
+++ training.py
@@ -5,24 +5,39 @@ import requests
 import zipfile
 import os
 import pickle
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score, classification_report
 from sklearn.ensemble import RandomForestClassifier
 
+
+comb_tab = {
+        'comb1' :[   'CALI', 'RHOB', 'GR', 'SGR', 'NPHI', 'PEF', 'SP', 'BS', 'DCAL', 'DRHO', 'MUDWEIGHT', 'RMIC', 'RXO', ],
+        'comb2' : [   'CALI', 'RHOB', 'GR', 'SGR', 'NPHI', 'PEF', 'DTC', 'SP', 'BS', 'DTS', 'DCAL', 'DRHO', 'MUDWEIGHT', 
+                'RMIC', 'RXO' ],
+        'comb3' : [   'CALI', 'RSHA', 'RMED', 'RDEP', 'RHOB', 'GR', 'SGR', 'NPHI', 'PEF', 'DTC', 'SP', 'BS', 'DTS', 'DCAL', 
+                'DRHO', 'MUDWEIGHT', 'RMIC', 'RXO'  ],
+        'comb4' : [   'CALI', 'RSHA', 'RMED', 'RDEP', 'RHOB', 'GR', 'SGR', 'NPHI', 'PEF', 'DTC', 'SP', 'BS', 'ROP', 'DTS', 
+                'DCAL', 'DRHO', 'MUDWEIGHT', 'RMIC', 'ROPA', 'RXO' ]
+    } 
+    
 ############### Loading data  ##################
 
 def load_data():
-    # ## Fetching the data from github
-    # url = 'https://github.com/bolgebrygg/Force-2020-Machine-Learning-competition/raw/master/lithology_competition/data/train.zip'
-    # r = requests.get(url, allow_redirects=True)
-    # if not os.path.exists('Data'):
-    #     os.mkdir('Data')
-    # open('Data/train.zip', 'wb').write(r.content)
+    if os.path.isfile('Data/train.csv'):  #Check if data is already available
+        pass
+    else:
+        ## Fetching the data from github
+        url = 'https://github.com/bolgebrygg/Force-2020-Machine-Learning-competition/raw/master/lithology_competition/data/train.zip'
+        r = requests.get(url, allow_redirects=True)
+        if not os.path.exists('Data'):
+            os.mkdir('Data')
+        open('Data/train.zip', 'wb').write(r.content)
 
-    # ## Unziping zip file
-    # with zipfile.ZipFile('Data/train.zip', 'r') as zip_ref:
-    #     zip_ref.extractall('Data/')
-
+        ## Unziping zip file
+        with zipfile.ZipFile('Data/train.zip', 'r') as zip_ref:
+            zip_ref.extractall('Data/')
+    
     ## Reading the data
     raw_data = pd.read_csv("Data/train.csv", sep = ';')
     lithology_keys = {30000: 'Sandstone',
@@ -37,22 +52,24 @@ def load_data():
                     99000: 'Tuff',
                     90000: 'Coal',
                     93000: 'Basement'}
-
-
     raw_data = raw_data.replace({"FORCE_2020_LITHOFACIES_LITHOLOGY": lithology_keys})
-
     raw_data.rename(columns={'FORCE_2020_LITHOFACIES_LITHOLOGY':'Lithofacies'}, inplace=True)
-
     raw_data.rename(columns={'FORCE_2020_LITHOFACIES_CONFIDENCE':'Confidence'}, inplace=True)
 
-    data_headers = [   'CALI', 'RSHA', 'RMED', 'RDEP', 'RHOB', 'GR', 'SGR', 'NPHI', 'PEF',
-        'DTC', 'SP', 'BS', 'ROP', 'DTS', 'DCAL', 'DRHO', 'MUDWEIGHT', 'RMIC',
-        'ROPA', 'RXO', 'Lithofacies', ]
+    return raw_data[comb_tab['comb4']],raw_data['Lithofacies']
 
-    return raw_data[data_headers]
+    
+# def exploratory_data_analysis(data,label): 
+#     data['Lithofacies'] = label
+#     def boxplot_sorted(df, by, column):
+#         df2 = pd.DataFrame({col:vals[column] for col, vals in df.groupby(by)})
+#         meds = df2.median().sort_values()
+#         df2[meds.index].boxplot(rot=90, grid = True, figsize = (20,10))
+#     for col in data.columns[:-1]: 
+#         boxplot_sorted(data, 'Lithofacies', col)
+#         plt.figure(figsize = (20,10))
+#         print(col)
 
-# def exploratory_data_analysis():
-#     pass
 
 
 def preprocessing(data):
@@ -81,119 +98,107 @@ def preprocessing(data):
     ####################### Removing NaN values  #############################
     def replace_nan(x):
         if pd.isnull(x):
-            return 999999
+            return 9999
         else:
             return x
-    
-    for header in data.columns[:-1]:
-        data[header] = data[header].apply(replace_nan) # Can it be replaces inplace?
-    
-    # print('Preprocessing done')
+    for header in data.columns:
+        data[header] = data[header].apply(replace_nan) 
     return data
 
-ct = {
-        'comb1' :[   'CALI', 'RHOB', 'GR', 'SGR', 'NPHI', 'PEF', 'SP', 'BS', 'DCAL', 'DRHO', 'MUDWEIGHT', 'RMIC', 'RXO', ],
-        'comb2' : [   'CALI', 'RHOB', 'GR', 'SGR', 'NPHI', 'PEF', 'DTC', 'SP', 'BS', 'DTS', 'DCAL', 'DRHO', 'MUDWEIGHT', 
-                'RMIC', 'RXO' ],
-        'comb3' : [   'CALI', 'RSHA', 'RMED', 'RDEP', 'RHOB', 'GR', 'SGR', 'NPHI', 'PEF', 'DTC', 'SP', 'BS', 'DTS', 'DCAL', 
-                'DRHO', 'MUDWEIGHT', 'RMIC', 'RXO'  ],
-        'comb4' : [   'CALI', 'RSHA', 'RMED', 'RDEP', 'RHOB', 'GR', 'SGR', 'NPHI', 'PEF', 'DTC', 'SP', 'BS', 'ROP', 'DTS', 
-                'DCAL', 'DRHO', 'MUDWEIGHT', 'RMIC', 'ROPA', 'RXO' ]
-    }
 
-
-def train(data,header,model_name):
+def train(data,label,comb_tab=comb_tab,comb='comb4',imp_plot=True):
     a = 0 
     b = 100
     test_sz = 9.99
 
-    X_train, X_test, Y_train, Y_test = train_test_split(data[header], data['Lithofacies'], random_state = int(b),
-                                                     test_size= (test_sz/10), stratify = data['Lithofacies'])
-    clf = RandomForestClassifier(n_estimators = 200, max_depth=70,  min_samples_split=2, min_samples_leaf=1,max_features=10, random_state=0)
+    header = comb_tab[comb]
+    X_train, X_test, Y_train, Y_test = train_test_split(data[header], label, random_state = int(b),
+                                                     test_size= (test_sz/10), stratify = label)
+    clf = RandomForestClassifier(n_estimators = 200, max_depth=70,  min_samples_split=2, 
+                                    min_samples_leaf=1,max_features=10, random_state=0)
     clf.fit(X_train, Y_train)
-    ## Saving model
+
+    ## Saving the model
     if not os.path.exists('Models'):
         os.mkdir('Models')
-    outfile = open('Models/'+model_name,'ab')
+    outfile = open('Models/Model_'+comb[-1],'ab')
     pickle.dump(clf,outfile)
     outfile.close()
+    print('Model has been saved at Models/Model_'+comb[-1])
 
+    ## Traiing results
     predicted_output = clf.predict(X_train)
     true_lithofacies = Y_train 
-
-    # cm = confusion_matrix(true_lithofacies, predicted_output)
-    # cmp = ConfusionMatrixDisplay(cm) 
-    # fig, ax = plt.subplots(figsize=(10,10))
-    # cmp.plot(ax=ax)
-    # plt.show()
-
-    # print("\nTraining Accuracy:", accuracy_score(true_lithofacies, predicted_output))
-    # print("\n -------------Training Classification Report-------------\n")
-    # print(classification_report(true_lithofacies, predicted_output))
-    return clf
-    # predicted_output = clf.predict(X_test)
-    # true_lithofacies = Y_test 
-
-    # cm = confusion_matrix(true_lithofacies, predicted_output)
-    # cmp = ConfusionMatrixDisplay(cm) # , display_labels=np.arange(25))
-    # fig, ax = plt.subplots(figsize=(10,10))
-    # cmp.plot(ax=ax)
-
-    # print(" test Accuracy:", accuracy_score(true_lithofacies, predicted_output))
-    # print("\n ------------- Test Classification Report-------------\n")
-    # print(classification_report(true_lithofacies, predicted_output))
+    cm = confusion_matrix(true_lithofacies, predicted_output)
+    cmp = ConfusionMatrixDisplay(cm) 
+    fig, ax = plt.subplots(figsize=(10,10))
+    cmp.plot(ax=ax)
+    plt.show()
+    print("\nTraining Accuracy:", accuracy_score(true_lithofacies, predicted_output))
+    print("\n -------------Training Classification Report-------------\n")
+    print(classification_report(true_lithofacies, predicted_output))
+    
+    ## Test results
+    predicted_output = clf.predict(X_test)
+    true_lithofacies = Y_test 
+    cm = confusion_matrix(true_lithofacies, predicted_output)
+    cmp = ConfusionMatrixDisplay(cm) # , display_labels=np.arange(25))
+    fig, ax = plt.subplots(figsize=(10,10))
+    cmp.plot(ax=ax)
+    print(" test Accuracy:", accuracy_score(true_lithofacies, predicted_output))
+    print("\n ------------- Test Classification Report-------------\n")
+    print(classification_report(true_lithofacies, predicted_output))
 
 
-
-def train_model1(data):
-    # print('Training started')
-    comb1 = ct['comb1']
-    return train(data,comb1,'Model_1')
-
-def feature_importance(clf,ct,comb,plot=True):
+    ### Feature Importance
     importances = clf.feature_importances_
     sorted_indices = np.argsort(importances)[::-1]
-    keys = ct[comb]
-    values = importances
-    imp_dict = {}
-    for i,col in enumerate(keys):
-        imp_dict[col] = values[i]
-    if plot:
-        plt.figure(figsize=(15,6))
-        plt.title('Feature Importance')
+    if imp_plot:
+        plt.figure(int(comb[-1]),figsize=(15,6))
+        plt.title('Feature Importance for '+comb)
         plt.xlabel('Logs')
         plt.ylabel('Relative importance')
-        plt.bar(range(len(ct[comb])), importances[sorted_indices], align='center')
-        plt.xticks(range(len(ct[comb])), np.array(ct[comb])[sorted_indices], rotation=90)
+        plt.bar(range(len(comb_tab[comb])), importances[sorted_indices], align='center')
+        plt.xticks(range(len(comb_tab[comb])), np.array(comb_tab[comb])[sorted_indices], rotation=90)
         plt.tight_layout()
-        plt.show()
-    if not os.path.exists('Models'):
-        os.mkdir('Models')
-    outfile = open('Models/imp_'+comb,'ab')
-    pickle.dump(imp_dict,outfile)
-    outfile.close()
-    return imp_dict
+        plt.savefig('Models/Feature_Imp'+comb[-1])
+        print('Figure for feature importance has been saved at Models/Feature_Imp'+comb[-1]+'.png')
 
 
-    ######################## 4 datasets for each combinations #########################
-    # 
-    # comb2 = [   'CALI', 'RHOB', 'GR', 'SGR', 'NPHI', 'PEF', 'DTC', 'SP', 'BS', 'DTS', 'DCAL', 'DRHO', 'MUDWEIGHT', 
-    #         'RMIC', 'RXO', ]
-    # comb3 = [   'CALI', 'RSHA', 'RMED', 'RDEP', 'RHOB', 'GR', 'SGR', 'NPHI', 'PEF', 'DTC', 'SP', 'BS', 'DTS', 'DCAL', 
-    #         'DRHO', 'MUDWEIGHT', 'RMIC', 'RXO',  ]
-    # comb4 = data_headers[:-1]
 
-    ####################### Training combination-1  ###########################
-    
-    
-    ## reading model
-    # file = open('Models/'+model_namemodel_name,'rb')
-    # clf = pickle.load(file)
-    # file.close()
-    
-    
 if __name__ == '__main__':
-    data = load_data()
+    data,label = load_data()
     data = preprocessing(data)
-    clf = train_model1(data)
-    feature_importance(clf,ct,comb='comb1')
+    # exploratory_data_analysis(data,label)
+
+    train(data,label,comb='comb1',imp_plot=True)
+    train(data,label,comb='comb2',imp_plot=True)
+    train(data,label,comb='comb3',imp_plot=True)
+    train(data,label,comb='comb4',imp_plot=True)
+
+
+
+
+# def feature_importance(clf,comb_tab,comb,plot=True):
+#     importances = clf.feature_importances_
+#     sorted_indices = np.argsort(importances)[::-1]
+#     keys = comb_tab[comb]
+#     values = importances
+#     imp_dict = {}
+#     for i,col in enumerate(keys):
+#         imp_dict[col] = values[i]
+#     if plot:
+#         plt.figure(figsize=(15,6))
+#         plt.title('Feature Importance')
+#         plt.xlabel('Logs')
+#         plt.ylabel('Relative importance')
+#         plt.bar(range(len(comb_tab[comb])), importances[sorted_indices], align='center')
+#         plt.xticks(range(len(comb_tab[comb])), np.array(comb_tab[comb])[sorted_indices], rotation=90)
+#         plt.tight_layout()
+#         plt.show()
+    # if not os.path.exists('Models'):
+    #     os.mkdir('Models')
+    # outfile = open('Models/imp_'+comb,'ab')
+    # pickle.dump(imp_dict,outfile)
+    # outfile.close()
+    # return imp_dict
